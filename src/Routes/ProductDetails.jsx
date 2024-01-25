561a9b66e99cbe44137026414babfb4778120f7b
Vyshnav.K <vyshnavk117@gmail.com>
2024-01-25 13:28:22 +0530

    Add scroll to top functionality in ProductDetails component

--- src/Routes/ProductDetails.jsx
+++ src/Routes/ProductDetails.jsx
@@ -1,7 +1,11 @@
+import { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import ProductOverviews from '../components/ProductOverviews';
 
 const ProductDetails = () => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, []);
   return (
     <div>
         <Navbar />
